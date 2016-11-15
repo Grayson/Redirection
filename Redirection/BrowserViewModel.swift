4cbf16151c87e900c1ae78cd83c61e6135d25ae1
Grayson <info@fromconcentratesoftware.com>
2016-11-14 21:05:20 -0500

    Adding BrowserViewModel

---
+++ Redirection/BrowserViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  BrowserViewModel.swift
+//  Redirection
+//
+//  Created by Grayson Hansard on 11/14/16.
+//  Copyright © 2016 From Concentrate Software. All rights reserved.
+//
+
+import AppKit
+
+class BrowserViewModel {
+	let image: NSImage
+	let title: String
+	let path: String
+
+	init(info: BrowserInfo) {
+		title = info.name
+		path = info.location.path
+		image = NSWorkspace.shared().icon(forFile: path)
+	}
+}
