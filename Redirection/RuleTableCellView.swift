efe787ad384cf19e9892f24bfbf51331b1026e66
Grayson <info@fromconcentratesoftware.com>
2016-11-16 08:54:47 -0500

    Add code to persist information to RuleViewModel

--- Redirection/RuleTableCellView.swift
+++ Redirection/RuleTableCellView.swift
@@ -9,10 +9,55 @@
 import AppKit
 
 class RuleTableCellView : NSTableCellView {
-	@IBOutlet var matchPopupButton: NSPopUpButton?
-	@IBOutlet var browserPopupButton: NSPopUpButton?
+	@IBOutlet var matchPopupButton: NSPopUpButton? {
+		didSet {
+			matchPopupButton?.target = self
+			matchPopupButton?.action = #selector(matchPopupDidChange(_:))
+		}
+	}
+
+	@IBOutlet var browserPopupButton: NSPopUpButton? {
+		didSet {
+			browserPopupButton?.target = self
+			browserPopupButton?.action = #selector(browserPopupDidChange(_:))
+		}
+	}
+
+	@IBOutlet override var textField: NSTextField? {
+		didSet {
+			textField?.target = self
+			textField?.action = #selector(textFieldDidChange(_:))
+		}
+	}
 
 	override var objectValue: Any? { didSet { updateValues(viewModel: objectValue as? RuleViewModel) } }
+
+	func textFieldDidChange(_ sender: AnyObject) {
+		guard
+			let rvm = objectValue as? RuleViewModel,
+			let textField = textField
+		else { return }
+
+		rvm.value = textField.stringValue
+	}
+
+	func browserPopupDidChange(_ sender: AnyObject) {
+		guard
+			let rvm = objectValue as? RuleViewModel,
+			let browserPopupButton = browserPopupButton
+		else { return }
+
+		rvm.browsers.forEach { $0.isSelected = false }
+		rvm.browsers[browserPopupButton.indexOfSelectedItem].isSelected = true
+	}
+
+	func matchPopupDidChange(_ sender: AnyObject) {
+		guard
+			let rvm = objectValue as? RuleViewModel,
+			let matchPopupButton = browserPopupButton
+		else { return }
+		rvm.matchIndex = matchPopupButton.indexOfSelectedItem
+	}
 
 	private func updateValues(viewModel: RuleViewModel?) {
 		guard
