237d4c61dc5af1d13a0e9f783b2cd3eba273e3a1
Grayson <info@fromconcentratesoftware.com>
2016-11-17 19:23:46 -0500

    Add `Change<T>` type to encapsulate data changes

---
+++ Redirection/Change.swift
@@ -0,0 +1,12 @@
+//
+//  Change.swift
+//  Redirection
+//
+//  Created by Grayson Hansard on 11/17/16.
+//  Copyright © 2016 From Concentrate Software. All rights reserved.
+//
+
+struct Change<T> {
+	let old: T
+	let new: T
+}
